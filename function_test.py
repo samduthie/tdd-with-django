e2b4a3e98cbcd6dd444ef1e4d60544483a66dd85
Sam Duthie <sam@gmail.com>
2017-10-08 11:24:58 +0100

    improved functional tests

--- function_test.py
+++ function_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from unittest import TestCase
 import unittest
 
@@ -15,6 +16,25 @@ class NewVisitorTest(TestCase):
 		self.browser.get('http://localhost:8000')
 		
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item',
+		)
+
+		inputbox.send_keys('Buy peacock feathers')
+
+		inputbox.send_keys(Keys.Enter)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+
 		self.fail('finish the test')
 
 if __name__ == '__main__':
