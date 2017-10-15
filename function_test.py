26433a6a6702dd6b4266835eebe8ccba101473b8
Sam Duthie <samduthie@gmail.com>
2017-10-15 15:44:21 +0100

    User Input Saving and displaying Functional test failing

--- function_test.py
+++ function_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows], )
+
 	def test_start_list_and_retrieve_it(self):
 		self.browser.get('http://localhost:8000')
 		
@@ -28,13 +33,14 @@ class NewVisitorTest(TestCase):
 		inputbox.send_keys('Buy peacock feathers')
 
 		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-                    "New to-do item does not appear in table"
-		)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+ 		
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		self.fail('finish the test')
 
