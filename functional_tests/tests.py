ba52edaab979a292b5bdc8c56566d0388bdc2aab
Sam Duthie <samduthie@gmail.com>
2017-10-21 13:15:28 +0100

    moved functional tests into seperate directory NewVisitorTest now inherits from django.test.LiveServerTestCase Fixed bug causing firefox test errors

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,24 +1,25 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from unittest import TestCase
 import unittest
+import time
 
-class NewVisitorTest(TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
 	
 	def tearDown(self):
 		self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('td')
-		self.assertIn(row_text, [row.text for row in rows], )
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_start_list_and_retrieve_it(self):
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -33,18 +34,19 @@ class NewVisitorTest(TestCase):
 		inputbox.send_keys('Buy peacock feathers')
 
 		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
- 		
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		time.sleep(1)
 
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		
 		self.fail('finish the test')
 
-		#test
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
