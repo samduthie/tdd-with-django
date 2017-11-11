ce74c98a76096069e678e029ff2df067758b1bac
Sam Duthie <samduthie@gmail.com>
2017-11-11 21:12:31 +0000

    Add jupter notebook setup

--- deploy-tools/fabfile.py
+++ deploy-tools/fabfile.py
@@ -1,19 +1,16 @@
 from fabric.contrib.files import append, exists, sed
-from fabric.api import env, local, run
+from fabric.api import env, local, run, settings
 
 import random
 
 REPO_URL = 'https://github.com/samduthie/tdd-with-django.git'
+JNOTEBOOK_REPO_URL = 'https://github.com/samduthie/jnotebooks.git'
 
 def provision():
-    if not (exists('/home/sam')):
-        run('useradd -m -s /bin/bash sam')
-        run('usermod -a -G sudo sam')
-        run('passwd sam')
-        run('sudo su sam')
-
-    run('sudo apt-get update')
-    run('sudo apt-get install nginx git python3 python3-pip')
+    #_create_user()
+    with settings(prompts={'Do you want to continue? [Y/n]': 'Y'}):
+	    run('sudo apt-get update')
+	    run('sudo apt-get install nginx git python3 python3-pip')
     run('sudo pip3 install virtualenv')
     print("provision completed...")
    
@@ -21,6 +18,11 @@ def provision():
         print("deployment started...")
         deploy()
         print("deployment finished...")
+   
+    if _query("install jupyter notebooks? "):
+        print("installing notebooks...")
+        setup_jupyter()
+
     print("provision finished")
 
 def deploy():
@@ -32,6 +34,24 @@ def deploy():
 	_update_virtualenv(source_folder)
 	_update_static_files(source_folder)
 	_update_database(site_folder, source_folder)
+
+def setup_config():
+	site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+	source_folder = site_folder + '/source'
+	_setup_site_config(source_folder, site_folder)
+
+
+def setup_jupyter():
+	_setup_jupyter()
+
+def _create_user():
+	#method not called from anywhere
+	#must be switched into from root
+    if not (exists('/home/%s' % env.user)):
+        run('useradd -m -s /bin/bash %s' % env.user)
+        run('usermod -a -G sudo %s' % env.user)
+        run('passwd %s' % env.user)
+        run('sudo su %s' % env.user)
 
 def _create_directory_structure_if_necessary(site_folder):
 	for subfolder in('database', 'static', 'virtualenv', 'source'):
@@ -87,3 +107,73 @@ def _query(query):
         return False
     print("Please only answer (y)es or (n)o")
     _query(query)
+
+def _setup_site_config(source_folder, site_folder):
+	run ('cd %s' % source_folder)
+	
+	run("sudo cp deploy-tools/gunicorn-upstart.template.conf \
+		/etc/init/gunicorn-superlists.conf")	
+
+	run("sudo cp deploy-tools/nginx.template.conf \
+		/etc/nginx/sites-available/superlists")
+
+	run("sudo ln -s /etc/nginx/sites-available/superlists-staging \
+		 /etc/nginx/sites-enabled/superlists-staging")
+
+	run("../virtualenv/bin/gunicorn --bind unix:/tmp/superlists.socket superlists.wsgi:application")
+
+	run("sudo service nginx reload")
+
+def _create_user_config():
+	#todo - grab configs from git
+	pass
+
+def _setup_jupyter():
+	if env.user is 'root':
+		folder = '%s' % env.user
+	else:
+		folder = 'home/%s' % env.user
+
+	'''todo
+	create alias
+	django shell
+
+	'''
+	#Sets up j notebook environment
+	#pulls j notebooks from repo
+	#Downloading Miniconda 64Bits for Linux
+	if not (exists('/%s/miniconda3' % (folder))):
+		with settings(prompts={'Do you want to continue [Y/n]? ': 'Y'}):
+			run("wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh")
+			#Changing file permission for execution
+			run("chmod a+x Miniconda3-latest-Linux-x86_64.sh")
+			#Installing Miniconda
+			run("./Miniconda3-latest-Linux-x86_64.sh")
+			run("source ~/.bashrc")
+	else:
+		print("conda already installed")
+	# Follow instructions to complete install
+	if not (exists ('/%s/miniconda3/envs/py3' % (folder))):
+		run("./miniconda3/bin/conda create -n py3 python=3 ipython")
+	else:
+		print("py3 profile already set up")
+	# Activating created environment
+	run("source /%s/miniconda3/bin/activate py3" % (folder))
+	# Install package manager pip
+	#run("/root/miniconda3/bin/conda install pip")
+	# which ipython is to be used in the environment? pip freeze shows it
+	#run("pip freeze")
+	# Installing ipython notebook
+	with settings(prompts={' Proceed ([y]/n)?' : 'y'}):
+		run("/%s/miniconda3/bin/conda install jupyter" % (folder))
+		# Installing the packages
+		run("/%s/miniconda3/bin/conda install numpy" % (folder))
+		run("/%s/miniconda3/bin/conda install pandas" % (folder))
+		run("/%s/miniconda3/bin/conda install matplotlib" % (folder))
+
+	if not (exists('/%s/jnotebooks' % env.user)):
+		run("git clone %s jnotebooks" % JNOTEBOOK_REPO_URL)
+		run("/%s/miniconda3/bin/conda/jupyter notebook --generate-config" % env.user)
+		
+	if _query("Start Jupyter Notebooks? "):
+		run("/%s/miniconda3/bin/conda/jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser --allow-root &" % env.user)
